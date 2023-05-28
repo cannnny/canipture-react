ec7ce0be21736e8d6ba21a084677d3ade3af05a7
cany <couleurvide1008@gmail.com>
2023-05-28 11:46:00 +0900

    Screenshotsの要素追加

--- src/routes/screenshots.js
+++ src/routes/screenshots.js
@@ -1,6 +1,13 @@
+import '../reset.css';
+import '../App.scss';
+import {Link} from 'react-router-dom';
+
 function Screenshots(){
-    return(
-        <h2>Screenshots</h2>
+    return (
+        <div>
+            <h2>Screenshots</h2>
+            <Link to="/">▷ Top</Link>
+        </div>  
     );
 }
 
