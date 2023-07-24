d758936e8782809199019337987b7ab6e465ccc2
cany <couleurvide1008@gmail.com>
2023-07-24 20:48:21 +0900

    ギャラリーを表示させる

--- src/components/totk.js
+++ src/components/totk.js
@@ -1,13 +1,25 @@
 import Header from "./Header";
 
 const Totk = () => {
-  // const [hoverIcon, setHoverIcon] = useState("");
-
   return (
     <>
       <Header />
       <div className="screenshots">
         <h2>ゼルダの伝説 ティアーズ オブ ザ キングダム</h2>
+      </div>
+      <div className="main-gallery">
+        {/* コンポーネント化 */}
+        {(() => {
+          const gallery = [];
+          for (let i = 1; i < 100; i++) {
+            gallery.push(
+              <img src={`../pictures/gl/totk${i}.jpg`} alt="" key={i} />
+            );
+          }
+          return gallery;
+        })()}
+        {/* スクロールに応じてサムネを表示させるようにしたい */}
+        {/* クリックしたら拡大させたい */}
       </div>
     </>
   );
