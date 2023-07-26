65104fcccc98a44816a2a2049c24ca2f8ebacb5e
cany <couleurvide1008@gmail.com>
2023-07-26 11:16:38 +0900

    不要な記述を削除

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -8,7 +8,6 @@ const Gallery = (props) => {
   const data = getData.contents;
   const currentData = data.find((picture) => picture.id === props.name);
   const currentPictures = currentData.pictures;
-  console.log(currentPictures);
 
   return currentPictures.map((element, index) => {
     return <img src={element.url} alt="" key={index} />;
