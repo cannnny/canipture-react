2bd0074230cf7ef6c534c102198ef6b1a5d87a9e
cany <couleurvide1008@gmail.com>
2023-07-30 18:48:45 +0900

    Routes.jsを再度追加

---
+++ src/Routes.js
@@ -0,0 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Page from "./components/Page";
+
+const RouterConfig = (props) => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home games={props.games} />} />
+      {props.games.map((element, index) => {
+        return (
+          <Route
+            key={index}
+            path={`/${element.name_en}`}
+            element={<Page title={element.title} path={element.name_en} />}
+          />
+        );
+      })}
+    </Routes>
+  );
+};
+
+export default RouterConfig;
