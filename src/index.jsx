d4ba4281e42746f00df52b13838fcea19e47a984
cany <couleurvide1008@gmail.com>
2023-08-02 15:23:00 +0900

    拡張子をjsからjsxに変更

---
+++ src/index.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "./reset.css";
+import "./App.scss";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import RouterConfig from "./Routes";
+
+const gameData = [
+  { name_en: "totk", title: "ゼルダの伝説 ティアーズ オブ ザ キングダム" },
+  { name_en: "spl", title: "Splatoon3" },
+  { name_en: "animalcrossing", title: "あつまれ どうぶつの森" },
+  { name_en: "mhsb", title: "モンスターハンター サンブレイク" },
+];
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <RouterConfig games={gameData} />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+reportWebVitals();
