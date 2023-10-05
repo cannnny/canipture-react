6c02e1e9e92dc5cab3392fecd7f0b86374c404c7
cany <couleurvide1008@gmail.com>
2023-10-05 23:35:46 +0900

    GameDataを別ファイルに独立させる

---
+++ src/components/GameData.js
@@ -0,0 +1,20 @@
+const GameData = [
+  {
+    title_en: "tears-of-the-kingdom",
+    title: "ゼルダの伝説 ティアーズ オブ ザ キングダム",
+    category_id: "74nz-7bnr",
+  },
+  { title_en: "splatoon3", title: "Splatoon3", category_id: "9d5whazdg86" },
+  {
+    title_en: "animal-crossing",
+    title: "あつまれ どうぶつの森",
+    category_id: "4i59gse0x",
+  },
+  {
+    title_en: "monster-hunter",
+    title: "モンスターハンター サンブレイク",
+    category_id: "4lueegvqdfy",
+  },
+];
+
+export default GameData;
