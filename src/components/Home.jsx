04ab799aa669ee69790d891c58e49261cad7ad62
cany <couleurvide1008@gmail.com>
2023-10-05 23:36:54 +0900

    microCMSからデータを読み込む処理の記述を整理

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,48 +1,30 @@
 import Slider from "./Slider.jsx";
-import { Client } from "./Client";
+import GameData from "./GameData.js";
+
+import { client } from "./Client";
+import { Link } from "react-router-dom";
 
 // カテゴリーごとに各々取得,slideにわたす
-const getData1 = await Client.get({
-  endpoint: "gallery",
-  queries: {
-    // カテゴリーの指定はカテゴリ名じゃなくidだった気がする、、microcmsの公式サイト要確認
-    filters: "isTopShow[equals]true",
-  },
-});
-const getData2 = await Client.get({
-  endpoint: "gallery",
-  queries: {
-    filters: "category[equals]splatoon3[and]isTopShow[equals]true",
-  },
-});
-const getData3 = await Client.get({
-  endpoint: "gallery",
-  queries: {
-    filters: "category[equals]animal-crossing[and]isTopShow[equals]true",
-  },
-});
-const getData4 = await Client.get({
-  endpoint: "gallery",
-  queries: {
-    filters: "category[equals]monster-hunter[and]isTopShow[equals]true",
-  },
-});
+const galleryData = [];
+for (let i = 0; i < GameData.length; i++) {
+  const data = await client.get({
+    endpoint: "gallery",
+    queries: {
+      filters: `category[equals]${GameData[i].category_id}[and]isTopShow[equals]true`,
+    },
+  });
+  galleryData.push(data.contents);
+}
 
-const Home = (props) => {
-  // 仮でgetData1のみ表示しているが、1,2,3,4を全て表示の必要あり
-  const data = getData1.contents;
-  console.log(data);
-
+const Home = () => {
   return (
     <div className="home">
       <div className="slider">
-        {props.games.map((element, index) => {
+        {GameData.map((element, index) => {
           return (
-            <Slider
-              key={element.title_en}
-              data={eval(`getData${index + 1}`).contents}
-              isReverse={index % 2 === 0}
-            />
+            <Link to={element.title_en} key={index}>
+              <Slider data={galleryData[index]} isReverse={index % 2 === 0} />
+            </Link>
           );
         })}
       </div>
